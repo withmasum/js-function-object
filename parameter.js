1b5e0bd775230eb3fa81f4098856f365677f2356
Masum Ahmed <creatormasum@gmail.com>
2022-08-07 18:06:39 +0600

    parameter end

--- parameter.js
+++ parameter.js
@@ -1,7 +1,36 @@
-function bringSingara(money) {
+/* function bringSingara(money) {
     console.log('mama singara den');
     console.log(money);
 }
 
 
-bringSingara(250); 
+bringSingara(250); */
+
+
+function bringSingara(money) {
+
+    console.log('taka disen: ', money)
+    console.log('ai nen singara');
+}
+
+var taka = 300;
+
+// bringSingara(taka);
+
+function add(num1, num2, num3) {
+    console.log('going to add: ', num1, num2, num3);
+}
+
+add(125, 96, 300);
+
+
+
+
+function sum(a, b, c, d, e) {
+
+    console.log(a, b, c, d, e);
+    var sub = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 5, 9);
