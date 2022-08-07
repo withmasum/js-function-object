fe7f7addc929018e3df35aed0a76bc7c6e0aad40
Masum Ahmed <creatormasum@gmail.com>
2022-08-07 19:27:22 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,33 @@
+var student = {
+    id: 115,
+    name: 'Masum Ahmed',
+    class: 9,
+    marks: 98
+
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '64 gb',
+    camera: '7MP'
+}
+
+
+var computer = {
+    brand: 'lenevo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7',
+
+}
+
+
+
+// console.log(computer);
+
+//got a sigle property 
+console.log(computer.brand);
+computer.processor = 'i79';
+console.log(computer.processor);
+console.log(mobile.price);
