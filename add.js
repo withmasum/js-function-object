e3ba865f1dc850d9da5e1cefb048ced5e1fdc955
Masum Ahmed <creatormasum@gmail.com>
2022-08-07 18:36:37 +0600

    add.js

---
+++ add.js
@@ -0,0 +1,15 @@
+function add(number1, number2) {
+
+    const sum = number1 + number2;
+    return sum;
+
+
+}
+
+const result1 = add(12, 13);
+const result2 = add(35, 7);
+const finalResult = add(result1, result2);
+
+console.log('here is my final result : ', finalResult)
+
+
